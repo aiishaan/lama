70c1691a4accba9838f184af14aee9b5936298c1
aishanirfan <aishanirfan6@gmail.com>
2024-06-20 19:49:00 +0530

    finished the basic navbar

---
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Link from "next/link"
+import styles from "./navLink.module.css"
+import { usePathname } from "next/navigation"
+
+export default function NavLink({item}) {
+
+    const pathName = usePathname();
+
+    return(
+       <Link className={`${styles.container} ${pathName=== item.path && styles.active}`} href={item.path} >{item.title}</Link>
+    )
+}
