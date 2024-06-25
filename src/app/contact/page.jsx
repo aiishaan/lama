cc7d32a125b2e150143179d47d3febb324a7457a
aishanirfan <aishanirfan6@gmail.com>
2024-06-25 17:16:33 +0530

    started contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,18 @@
+import Image from 'next/image'
+import styles from './contact.module.css'
+
 function ContactPage () {
     return (
-        <div>ContactPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src='/contact.png' alt='' fill/>
+            </div>
+            <div className={styles.formContainer}>
+                <form action="" className={styles.form}>
+                    <input type="text" placeholdername="" id="" />
+                </form>
+            </div>
+        </div>
     )
 }
 
