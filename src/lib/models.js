cd9429f24243df892e5befac1213d5030c31e953
aishanirfan <aishanirfan6@gmail.com>
2024-06-27 14:51:21 +0530

    defined schemas

--- src/lib/models.js
+++ src/lib/models.js
@@ -29,3 +29,31 @@ const userSchema = new mongoose.Schema({
 }, {timestamps: true}
 );
 
+const postSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required: true,
+        unique: true,
+        min: 3,
+        max: 20
+    },
+    email : {
+        type : String,
+        required: true,
+        unique: true,
+        max: 50,
+    },
+    password : {
+        type : String,
+        required: true,
+        min: 6,
+    },
+    img : {
+        type: String,
+    },
+    isAdmin : {
+        type: Boolean,
+        default : false,
+    },
+}, {timestamps: true}
+);
