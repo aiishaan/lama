8792a7712546b1778e9e7ad0066be5e4282e4f03
aishanirfan <aishanirfan6@gmail.com>
2024-07-02 17:30:54 +0530

    setup auth js config

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,3 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
-export const { handlers, auth } = NextAuth({ providers: [ GitHub ] })
+export const { handlers, auth, signIn, signOut } = NextAuth({ providers: [ GitHub({clientId:process.env.GITHUB_ID, clientSecret:process.env.GITHUB_SECRET}) ] })
