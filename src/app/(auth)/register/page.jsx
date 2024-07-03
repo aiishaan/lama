08e6176d79d77578a484bd16f00e09525653e5b0
aishanirfan <aishanirfan6@gmail.com>
2024-07-03 16:21:37 +0530

    updated register form styles

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -3,15 +3,16 @@ import styles from "./register.module.css"
 
 function RegisterPage() {
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
             <form className={styles.form} action={register}>
                 <input type="text" placeholder="username" name="username" />
                 <input type="email" placeholder="email" name="email" />
                 <input type="password" placeholder="password" name="password" />
                 <input type="password" placeholder="retype password" name="passwordrepeat" />
                 <button>Register</button>
-
             </form>
+            </div>
         </div>
     )
 }
