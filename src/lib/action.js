86097c16833d96af801fd7d73cad2d49279fdae0
aishanirfan <aishanirfan6@gmail.com>
2024-07-03 16:55:21 +0530

    added hashing and salting to passwords

--- src/lib/action.js
+++ src/lib/action.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { Post, User } from "./models";
 import { connectToDb } from "./utils";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcrypt"
 
 export const addPost = async (formData) => {
 
@@ -53,8 +54,6 @@ export const handleLogout = async () => {
 
 export const register = async (formData) => {
     const {username, email, password, img, passwordRepeat} = Object.fromEntries(formData);
-    console.log(passwordRepeat)
-    console.log(formData)
 
     if(password !== passwordRepeat){
         return {error: "Password do not match"}
@@ -68,10 +67,13 @@ export const register = async (formData) => {
             return "Username already exists";
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const newUser = new User({
             username,
             email,
-            password,
+            password: hashedPassword,
             img,
         });
 
